5a5755caf06446886dc11d34c64149dd9b57ad95
asnopek <asnopek486@gmail.com>
2020-03-28 18:16:16 -0500

    Changed CreatePost() so it works.

--- Models/BloggingContext.cs
+++ Models/BloggingContext.cs
@@ -1,4 +1,7 @@
-﻿using System.Data.Entity;
+﻿using NLog;
+using System;
+using System.Data.Entity;
+using System.Linq;
 
 namespace BlogsConsole.Models
 {
@@ -9,10 +12,73 @@ namespace BlogsConsole.Models
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<Post> Posts { get; set; }
 
-        public void AddBlog(Blog blog)
+
+        public void DisplayBlogs()
         {
-            this.Blogs.Add(blog);
+            var query = Blogs.ToList();
+            Console.WriteLine("All Blogs in the database: ");
+            Console.WriteLine("{0}-{1}", "Blog ID", "Blog Name");
+
+            foreach(var x in query)
+            {
+                Console.WriteLine("{0}-{1}", x.BlogId, x.Name);
+            }
+            Console.WriteLine("\n");
             this.SaveChanges();
+        }
+
+        public void DisplayPosts()
+        {
+            var query = Posts.ToList();
+            Console.WriteLine("All the Posts in the database: ");
+            Console.WriteLine("{0}-{1}-{2}", "Post ID", "Post Title", "Post Content");
+
+            foreach(var x in query)
+            {
+                Console.WriteLine("{0}-{1}-{2}", x.PostId, x.Title, x.Content);
+            }
+        }
+
+        public void AddBlogs()
+        {
+            Blog x = new Blog();
+            Console.WriteLine("Enter in the new Blog name: ");
+            x.Name = Console.ReadLine();
+
+            this.Blogs.Add(x);
+            this.SaveChanges();
+            Console.WriteLine("\n");
+        }
+
+        public void CreatePost()
+        {
+            DisplayBlogs();
+            Console.WriteLine("Choose a Blog you would like to add a post to by ID: ");
+            int choice = Int32.Parse(Console.ReadLine());
+
+            try
+            {
+                
+                var blog = this.Blogs.FirstOrDefault(t => t.BlogId == choice);
+                Post x = new Post();
+                x.BlogId = blog.BlogId;
+
+                Console.WriteLine("Enter in a new Post Title: ");
+                x.Title = Console.ReadLine();
+
+                 
+                Console.WriteLine("Enter the new Post Content: ");
+                x.Content = Console.ReadLine();
+
+                this.Posts.Add(x);
+                this.SaveChanges();
+                Console.WriteLine("\n");
+            }
+            catch
+            {
+                Console.WriteLine("This is not a valid ID");
+            }
+          
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,34 +10,79 @@ namespace BlogsConsole
         private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
         public static void Main(string[] args)
         {
-            logger.Info("Program started");
-            try
+            //logger.Info("Program started");
+            //try
+            //{
+
+            bool inProgram = true;
+
+            do
             {
+                Console.WriteLine("Enter your selection: ");
+                Console.WriteLine("1) Display all Blogs");
+                Console.WriteLine("2) Add Blog");
+                Console.WriteLine("3) Create Post");
+                Console.WriteLine("4) Display Posts");
+                Console.WriteLine("Enter q to quit");
+                string response = Console.ReadLine();
 
-                // Create and save a new Blog
-                Console.Write("Enter a name for a new Blog: ");
-                var name = Console.ReadLine();
-
-                var blog = new Blog { Name = name };
-
-                var db = new BloggingContext();
-                db.AddBlog(blog);
-                logger.Info("Blog added - {name}", name);
-
-                // Display all Blogs from the database
-                var query = db.Blogs.OrderBy(b => b.Name);
-
-                Console.WriteLine("All blogs in the database:");
-                foreach (var item in query)
+                switch (response)
                 {
-                    Console.WriteLine(item.Name);
+                    case "1":
+                        {
+                            var db = new BloggingContext();
+                            db.DisplayBlogs();
+                            db.SaveChanges();
+                            break;
+                        }
+                    case "2":
+                        {
+                            var db = new BloggingContext();
+                            db.AddBlogs();
+                            db.SaveChanges();
+                            break;
+                        }
+                    case "3":
+                        {
+                            var db = new BloggingContext();
+                            db.CreatePost();
+                            db.SaveChanges();
+                            break;
+                        }
+                    case "4":
+                        {
+                            var db = new BloggingContext();
+                            db.DisplayBlogs();
+                            db.SaveChanges();
+                            break;
+                        }
+                    case "q":
+                        {
+                            inProgram = false;
+                            break;
+                        }
                 }
-            }
-            catch (Exception ex)
-            {
-                logger.Error(ex.Message);
-            }
-            logger.Info("Program ended");
+
+            } while (inProgram);
+
+
+
+            //    // Display all Blogs from the database
+            //    var query = db.Blogs.OrderBy(b => b.Name);
+
+            //    Console.WriteLine("All blogs in the database:");
+            //    foreach (var item in query)
+            //    {
+            //        Console.WriteLine(item.Name);
+            //    }
         }
+        //catch (Exception ex)
+        //{
+        //    logger.Error(ex.Message);
+        //}
+        //logger.Info("Program ended");
     }
 }
+
+//    }
+//}
